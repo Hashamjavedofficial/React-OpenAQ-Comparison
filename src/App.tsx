4340ab7ce3800898b51c95d41ae05de24044af63
Hasham Javed <hasham.hasham1@gmail.com>
2023-01-30 04:49:20 +0500

    Project finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react';
+import {Dimmer, Grid, Loader, Segment} from 'semantic-ui-react'
 
-function App() {
+import Inputs from "./components/Inputs";
+import useInputs from "./hooks/useInputs";
+import {OpenQAApis} from "./Apis";
+
+import {Countries} from "./types";
+
+import './App.css'
+
+
+
+const App:React.FC = () =>  {
+    const [countries,setCountries] = useState<Countries[]>([])
+    const [parameters,setParameters] = useState<string[]>([])
+    const [isLoading,setIsLoading] = useState<boolean>(false)
+    const firstCity = useInputs()
+    const secondCity = useInputs()
+
+    useEffect(()=>{
+        setIsLoading(true)
+        OpenQAApis.fetchParameters().then((params:string[])=>{
+            setParameters(params)
+            setIsLoading(false)
+        }).catch(()=>{
+            setIsLoading(false)
+        })
+        OpenQAApis.fetchAllCountries().then((countries:Countries[])=>{
+            setCountries(countries)
+            setIsLoading(false)
+        }).catch(()=>{
+            setIsLoading(false)
+        })
+    },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+         {isLoading && <Segment>
+             <Dimmer active>
+                 <Loader size='massive'>Loading</Loader>
+             </Dimmer>
+         </Segment>}
+       <div className='heading-container'>
+         <h2>Air Quality Comparison</h2>
+       </div>
+       <Grid>
+         <Grid.Row columns={2}>
+           <Grid.Column>
+             <Inputs params={parameters} setIsLoading={setIsLoading} countries={countries} changeCountry={firstCity.changeCountry} selectedCountry={firstCity.selectedCountry} selectedCity={firstCity.selectedCity} changeCity={firstCity.changeCity} />
+           </Grid.Column>
+           <Grid.Column>
+               <Inputs params={parameters} setIsLoading={setIsLoading} countries={countries} changeCountry={secondCity.changeCountry} selectedCountry={secondCity.selectedCountry} selectedCity={secondCity.selectedCity} changeCity={secondCity.changeCity} />
+           </Grid.Column>
+         </Grid.Row>
+       </Grid>
+     </>
   );
 }
 
